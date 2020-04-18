2c8131dbb92261b0644436dbf82621008efe9bd4
unknown <venkatilla16@gmail.com>
2020-04-18 10:33:26 +0530

    task done

---
+++ Employee.java
@@ -0,0 +1,50 @@
+public class Employee {
+   
+   private String name;
+   private Integer id;
+   private Integer age;
+   
+   
+   
+   public Employee(){
+   
+   
+   }
+   public Employee(String name, Integer id, Integer age){
+   
+   this.name= name;
+   this.id=id;
+   this.age=age;
+   
+   
+   }
+   public void setName(String name){
+   this.name=name;
+   
+   }
+   public void setId(Integer id){
+   this.id=id;
+   
+   }
+   
+   public void setName(Integer age){
+   this.age=age;
+   
+   }
+   public String getName(){
+   return name;
+   
+   }
+    public Integer getId(){
+   return id;
+   
+   }
+    public Integer getAge(){
+   return age;
+   
+   }
+   
+   
+   
+
+}
