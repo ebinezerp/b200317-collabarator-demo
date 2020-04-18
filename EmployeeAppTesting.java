2eca25b1d4058c22980df9e0630f552bfcbb3ba3
ebinezer.p87@gmail.com <ebinezer.p87@gmail.com>
2020-04-18 10:52:14 +0530

    suggestion to akhil

---
+++ EmployeeAppTesting.java
@@ -0,0 +1,26 @@
+public EmployeeAppTesting{
+	
+	
+	public static void testEmployeeNameComparator(){
+		
+		Employee emp1 = new Employee(101,"ravi",35);
+		Employee emp2 = new Employee(102, "kishore", 55);
+		
+		EmployeeNameComparator comparator = new EmployeeNameComparator();
+		int value = comparator.compare(emp1,emp2);
+		
+		assert value<0 : "Wrong implementation";
+		
+		System.out.println("Test successful");
+		
+	}
+	
+	
+	public static void main(String[] args){
+		
+		
+		testEmployeeNameComparator();
+		
+	}
+	
+}
